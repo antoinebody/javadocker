c9cb6501c66f9f2495c30a4857fe0319bc3315af
Antoine Body <antoine.body@gmail.com>
2015-12-30 21:59:32 +0100

    bonjour papa

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,6 @@
 public class JavaHelloWorld{
 	public static void main(String[] args){
-	   System.out.println("Java Hello World");
+	   System.out.println("Java Dad");
 	   System.out.println("Hello again");
            System.out.println("Trigeered !!!!");
 
